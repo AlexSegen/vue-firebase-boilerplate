83701b2f58fb40f725a4104f11456015287f5fc6
Alejandro Vivas <alejandrojv17@gmail.com>
2018-08-02 11:41:21 -0400

    Source links

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>Source</p>
+    <ul>
+      <li>
+        <a href="https://github.com/vuejs/vuefire" target="_blank">VueFire Documentation</a>
+      </li>
+      <li>
+        <a href="https://alligator.io/vuejs/vuefire-firebase/" target="_blank">Vue.js Firebase Integration with VueFire</a>
+      </li>
+    </ul>
   </div>
 </template>
